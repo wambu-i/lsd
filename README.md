7d44c2600334576ef9dd5b562e55f521830eca8f
Wambui Karuga <wambui.karugax@gmail.com>
2020-08-12 15:56:46 +0300

    lsd/lua: add utility for displaying low signal devices.
    Signed-off-by: Wambui Karuga <wambui.karugax@gmail.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+### Low Signal Disconnect (Lua)
+
+This utility provides information on how many AP clients connected to the Wi-Fi device have a dBm signal above a settable threshold.
+The information is updated periodically after a configurable interval.
+
+Usage:
+``` luajit lsd.lua <interface> <interval> <threshold>```
---
+++ lsd.lua
@@ -0,0 +1,48 @@
+require "socket"
+
+local iwinfo = require("iwinfo")
+local uci = require("uci")
+local log = require 'log'
+
+local args = {...}
+local iface = args[1]
+local interval = args[2]
+local threshold = tonumber(args[3])
+
+function get_connected_stations(iface)
+	local api = iwinfo.type(iface)
+   	local iw = iwinfo[api]
+	local tmp = 0
+	local str = ""
+	local stations = iw.assoclist(iface)
+	if not stations or next(stations) == nil then
+		str = string.format("%s: no associated stations", iface)
+		log.error(str)
+		return nil
+	end
+	for addr, info in pairs(stations) do
+		str = string.format("Client: %s - Signal: %s", addr, info.signal)
+		log.info(str)
+		if math.abs(info.signal) < threshold then tmp = tmp + 1 end
+	end
+	if tmp ~= 0 then
+		str = string.format("%d client(s) with less than %d signal", tmp, threshold)
+		log.info(str)
+	end
+end
+
+function get_devices_signal()
+	local now = socket.gettime()
+	local next_poll = now + interval
+	get_connected_stations(iface)
+	local secs = next_poll - now
+	if secs > 0 then socket.sleep(secs) end
+end
+
+while true do
+	local _, err = pcall(function () get_devices_signal() end)
+	if err ~= nil then
+		log.error(err)
+		break
+	end
+end
